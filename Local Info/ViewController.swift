8e8f4b10b758d36576a1aba299d75a23f5b0914f
Leslie <leslie.appl@gmail.com>
2020-03-31 12:40:38 +0800

    Initial Commit

---
+++ Local Info/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Local Info
+//
+//  Created by leslie on 3/31/20.
+//  Copyright © 2020 leslie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
